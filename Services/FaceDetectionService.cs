d1ffe2a4ff738d9c22b1b14ad118c706902fca5b
carol <carol@RED>
2025-10-08 19:31:01 -0300

    #

--- Services/FaceDetectionService.cs
+++ Services/FaceDetectionService.cs
@@ -1,78 +1,56 @@
-﻿#if ANDROID
-using Android.Gms.Tasks;
-using Android.Graphics;
-using Google.MLKit.Vision.Common;
-using Google.MLKit.Vision.Face;
-using Java.Util; // Importante para a lista de rostos
-
-#endif
+﻿using System.Text.Json;
+using System.Text;
 
 namespace SAAD2.Services
 {
     public class FaceDetectionService
     {
+        private readonly string _apiKey = "YOUR_API_KEY"; // Substitua pela sua chave da Vision API
+
         public async Task<(bool hasFace, string message)> DetectFaceAsync(Stream photoStream)
         {
-#if ANDROID
-            try
+            using var ms = new MemoryStream();
+            await photoStream.CopyToAsync(ms);
+            var base64Image = Convert.ToBase64String(ms.ToArray());
+
+            var requestBody = new
             {
-                var bitmap = await BitmapFactory.DecodeStreamAsync(photoStream);
-                if (bitmap == null) return (false, "Não foi possível ler a imagem.");
-
-                var image = InputImage.FromBitmap(bitmap, 0);
-                var options = new FaceDetectorOptions.Builder()
-                    .SetPerformanceMode(FaceDetectorOptions.PerformanceModeFast)
-                    .Build();
-
-                var detector = FaceDetection.GetClient(options);
-                var result = await detector.Process(image).ToMauiTask();
-
-                var faces = result as IList;
-
-                // Para listas Java, usamos a propriedade .Size() em vez de .Count
-                if (faces != null && faces.Size() > 0)
+                requests = new[]
                 {
-                    if (faces.Size() == 1) return (true, "Rosto detetado com sucesso!");
-
-                    return (false, $"Foram detetados {faces.Size()} rostos. Por favor, tire uma foto com apenas uma pessoa.");
+                    new
+                    {
+                        image = new { content = base64Image },
+                        features = new[] { new { type = "FACE_DETECTION", maxResults = 5 } }
+                    }
                 }
+            };
 
-                return (false, "Nenhum rosto foi detetado na imagem. Tente novamente.");
-            }
-            catch (Exception ex)
-            {
-                return (false, $"Erro técnico na deteção: {ex.Message}");
-            }
-#else
-            return await Task.FromResult((false, "A deteção de rosto só está disponível para Android."));
-#endif
+            var json = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using var client = new HttpClient();
+            var response = await client.PostAsync(
+                $"https://vision.googleapis.com/v1/images:annotate?key={_apiKey}",
+                content
+            );
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+                return (false, $"Erro na API: {response.StatusCode}");
+
+            var result = JsonDocument.Parse(responseJson);
+            var faceAnnotations = result.RootElement
+                .GetProperty("responses")[0]
+                .TryGetProperty("faceAnnotations", out var faces) ? faces : default;
+
+            if (faces.ValueKind != JsonValueKind.Array || faces.GetArrayLength() == 0)
+                return (false, "Nenhum rosto detectado.");
+
+            if (faces.GetArrayLength() == 1)
+                return (true, "Rosto detectado com sucesso!");
+
+            return (false, $"Foram detectados {faces.GetArrayLength()} rostos. Por favor, envie uma imagem com apenas uma pessoa.");
         }
     }
-
-#if ANDROID
-    public static class TaskExtensions
-    {
-        public static Task<Java.Lang.Object> ToMauiTask(this Android.Gms.Tasks.Task task)
-        {
-            var tcs = new TaskCompletionSource<Java.Lang.Object>();
-            task.AddOnSuccessListener(new OnSuccessListener(result => tcs.SetResult(result)));
-            task.AddOnFailureListener(new OnFailureListener(ex => tcs.SetException(ex)));
-            return tcs.Task;
-        }
-
-        private class OnSuccessListener : Java.Lang.Object, IOnSuccessListener
-        {
-            private readonly Action<Java.Lang.Object> _action;
-            public OnSuccessListener(Action<Java.Lang.Object> action) => _action = action;
-            public void OnSuccess(Java.Lang.Object result) => _action?.Invoke(result);
-        }
-
-        private class OnFailureListener : Java.Lang.Object, IOnFailureListener
-        {
-            private readonly Action<Java.Lang.Exception> _action;
-            public OnFailureListener(Action<Java.Lang.Exception> action) => _action = action;
-            public void OnFailure(Java.Lang.Exception e) => _action?.Invoke(e);
-        }
-    }
-#endif
 }
