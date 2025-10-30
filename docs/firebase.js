e344ef2e79dd6f77087cc68a02bc74773404ed0c
Necrovale1 <51262672+necrovale1@users.noreply.github.com>
2025-10-30 01:54:17 -0300

    Add files via upload

---
+++ docs/firebase.js
@@ -0,0 +1,14 @@
+// Configuração SAAD - Sistema de Autenticação Acadêmica Digital
+var firebaseConfig = {
+  apiKey: "AIzaSyCW4PQCcScohZJTo4IfevkCRxxXbmQY7HA",
+  authDomain: "saad-1fd38.firebaseapp.com",
+  projectId: "saad-1fd38",
+  storageBucket: "saad-1fd38.firebasestorage.app",
+  messagingSenderId: "688598210140",
+  appId: "1:688598210140:android:9d16f60634f1f2f1a1f899",
+  databaseURL: "https://saad-1fd38-default-rtdb.firebaseio.com"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+console.log("🔥 SAAD Firebase inicializado");
